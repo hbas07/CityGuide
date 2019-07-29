02cf35857b67a16c425fe73a29eecd2cac443465
hbas07 <hbas0798@gmail.com>
2019-07-29 16:34:21 +0300

    First Commit

---
+++ SehirRehberi.API/SehirRehberi.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using SehirRehberi.API.Dto;
+using SehirRehberi.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Helpers
+{
+    public class AutoMapperProfiles:Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<City, CityForListDto>()
+                .ForMember(dest=>dest.PhotoUrl,opt=>
+                {
+                    opt.MapFrom(src => src.photo.FirstOrDefault(p => p.IsMain).Url);
+                });
+
+            CreateMap<City, CityForDetailDto>();
+            CreateMap<PhotoForCreationDto,Photo>();
+            CreateMap<PhotoForReturnDto, Photo>();
+        }
+    }
+}
